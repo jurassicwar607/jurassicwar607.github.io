a52bddf5ab39ea7332a827f3dd464d5c57f92872
jurassicwar607 <56867119+jurassicwar607@users.noreply.github.com>
2019-10-23 20:11:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('player').addEventListener("mouseover",sumaPunts);
+
+punts = 0;
+temps = 60;
+necessari = 45;
+function sumaPunts(){
+  punts++;
+  document.getElementById("punts").innerHTML = "Punts: <b>" + punts + "/" + necessari + "</b>";
+  randNum = Math.round(Math.random()*500);
+  randNum2 = Math.round(Math.random()*500);
+  
+  document.getElementById("player").style.marginTop =randNum + "px";
+  document.getElementById("player").style.marginLeft =randNum2 + "px";
+  if (punts == 45) {
+    alert("T'HA COSTAT PERO MIRA COM HO HAS ACONSEGUIT DEMOSTRA QUE ETS MES LLEST QUE UN ORANGUTAN");
+    punts = 0;
+    temps = 60;
+    necessari+= 10;
+  }
+}
+
+function restaTemps() {
+  temps--;
+  document.getElementById("temps").innerHTML = "&nbsp;&nbsp;&nbsp;Temps: "+temps;
+  if (temps == 0) {
+    alert("60 SEGUNDOS Y NO LO COMPLETAS COMPA TENDRAS UNA MIERDA DE FUTURO :)")
+    temps = 60;
+    punts = 0;
+  }
+}
+
+setInterval(restaTemps,1000);
